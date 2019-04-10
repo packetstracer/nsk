23b6d3b04018c0d5fa43573d4734696438428859
Iván Merín Rodríguez <ivmerod@yahoo.es>
2019-04-10 15:20:15 +0200

    feature(server/mongo): Add error and connect handlers

--- src/server/mongodb-server.js
+++ src/server/mongodb-server.js
@@ -4,10 +4,18 @@ const mongoose = require('mongoose')
 const mongodbServer = {
   connectDb: function (conf) {
     try {
-      mongoose.connect(conf.url + '/' + conf.dbName)
+      mongoose.connect(`${conf.url}/${conf.dbName}`, { useNewUrlParser: true })
+
+      const db = mongoose.connection
+
+      db.on('error', console.error.bind(console, 'connection error:'))
+
+      db.once('open', function() {
+        console.log('NSK Connected to MongoDB Server')
+      })
     }
     catch (e) {
-      console.log('Api router Error: %s', e.message)
+      console.log('MongoDB Connection Error: %s', e.message)
       throw(e)
     }
   }
