9f16f594c88eb8f19adcd3bd907757638653ed66
Iván Merín Rodríguez <ivmerod@yahoo.es>
2019-04-10 15:21:26 +0200

    chore(i18n): Format file

--- src/lang/api-msg.js
+++ src/lang/api-msg.js
@@ -1,7 +1,6 @@
 const messages = {
-    'error_api_wrong_content': 'wrong body content',
-    'msg_api_instance_created': 'instance created',
-    // '': '',
+  'error_api_wrong_content': 'wrong body content',
+  'msg_api_instance_created': 'instance created'
 }
 
 module.exports = messages
