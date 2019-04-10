b240b88a09ace43c481fcd42db62eadb0e51ff2e
Iván Merín Rodríguez <ivmerod@yahoo.es>
2019-04-10 15:28:53 +0200

    fix(api): Fix Bear MongoDB Model API endpoints

--- src/routes/api/router.js
+++ src/routes/api/router.js
@@ -1,39 +1,35 @@
 const express = require('express')
 
-const Bear   = require('../../app/models/bear')
-const rskApi = require('../../app/libs/rsk-api')
-
+const apiMsg = require('../../lang/api-msg')
+const Bear = require('../../app/models/bear')
 
 const router = express.Router()
 
 // route middleware for pre processing
 router.use(function (req, res, next) {
-    console.log('API request: ' + req.method + ' ' + req.originalUrl)
+    console.log(`API request: ${req.method} ${req.originalUrl}`)
     next()
   }
 )
-
 
 // API Route Handling
 router.get('/', function (req, res) {
     res.json({
       status: 200,
-      message: 'Node Server Kit -> API response OK!!!'
+      message: 'NSK -> API response OK!!!'
     })
   }
 )
-
 
 router.get('/test', function (req, res) {
     res.json({
       status: 200,
-      message: 'Node Server Kit -> API Test Endpoint response OK!!!'
+      message: 'NSK -> API Test Endpoint response OK!!!'
     })
   }
 )
 
-
-// Oso model CRUD
+// Bear model CRUD
 router
   .route('/bears')
 
@@ -48,101 +44,88 @@ router
     }
   )
 
-
   .post(function (req, res) {
-    const apiMsg = require('./src/messages/api-msg')
-
     if (!req.body.name) {
-      res.json({error: apiMsg.error_api_wrong_content})
+      res.json(400, { error: apiMsg.error_api_wrong_content })
       return false
     }
 
-    let bear  = new Bear()
+    let bear = new Bear()
     bear.name = req.body.name
     bear.save(function (error) {
         if (error) {
           res.send(error)
         }
 
-        res.json({message: apiMsg.msg_api_instance_created})
+        res.json({ message: apiMsg.msg_api_instance_created })
       }
     )
   })
 
-
   .put(function (req, res) {
     if (!req.body.id) {
-      res.json({error: 'wrong body content'})
+      res.json(400, { error: 'wrong body content' })
       return false
     }
 
     Bear.findById(req.body.id, function (error, bear) {
         if (error) {
-          res.json(404, {error: 'instance not found'})
+          res.json(404, { error: 'instance not found' })
         }
 
-        bear.name = req.body.data.name
+        bear.name = req.body.name
 
-        bear.save(function (error, res) {
+        bear.save(function (error) {
             if (error) {
               res.send(error)
             }
 
-            res.json({message: 'instance saved'})
+            res.json({ message: 'instance saved' })
           }
         )
       }
     )
   })
 
-
   .delete(function (req, res) {
     if (!req.body.name) {
-      res.json({error: 'wrong body content'})
+      res.json(400, { error: 'wrong body content' })
       return false
     }
 
-    Bear.remove({name: req.body.name}, function (error, result) {
+    Bear.deleteOne({ name: req.body.name }, function (error, result) {
         if (error) {
           res.send(error)
         }
 
         res.json(
           (function (result) {
-            const CODE = {
-              ok: 'message',
-              ko: 'error'
-            }
-
-            const MESSAGE = {
-              ok: 'instance(s) deleted',
-              ko: 'no instance found'
-            }
+            const CODE = { ok: 'message', ko: 'error' }
+            const MESSAGE = { ok: 'instance(s) deleted', ko: 'no instance found' }
 
             const res = (result.n > 0) ? 'ok' : 'ko'
-            let o     = {}
+            let o = {}
 
             o[CODE[res]] = MESSAGE[res]
 
             return o
-          })(result.result)
+          })(result)
         )
       }
     )
   })
 
-
 router
-  .route('/bears/:bear_id')
+  .route('/bears/:bearId')
   .get(function (req, res) {
-    if (!req.params.bear_id) {
-      res.json({error: 'wrong instance id'})
+    if (!req.params.bearId) {
+      res.json(400, { error: 'instance id not provided' })
       return false
     }
 
     Bear.findById(
-      req.params.bear_id,
-      {_id: 0, name: 1},
+      req.params.bearId,
+      { _id: 0, name: 1 },
 
       function (error, bear) {
         if (error) {
@@ -153,6 +136,5 @@ router
       }
     )
   })
-
 
 module.exports = router
